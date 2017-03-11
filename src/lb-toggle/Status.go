cfb834a056bf5ca181174b79333d22875b792631
Joe Vacovsky <vacovsky@gmail.com>
2017-03-11 15:17:05 -0800

    Fixed error handling bug

--- src/lb-toggle/Status.go
+++ src/lb-toggle/Status.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 )
@@ -26,30 +25,32 @@ type Status struct {
 func (s *Status) checkAppSmoke() {
 	r, err := http.Get(SETTINGS.Target.SmokeEndpoint)
 	if err != nil {
-		fmt.Println(err.Error())
-	}
-	if r.StatusCode == 200 {
-		STATUS.SmokeStatus.OK = true
-		// bodyBytes, err2 := ioutil.ReadAll(r.Body)
-		// bodyString = string(bodyBytes)
-	} else {
 		STATUS.SmokeStatus.OK = false
+	} else {
+		if r.StatusCode == 200 {
+			STATUS.SmokeStatus.OK = true
+			// bodyBytes, err2 := ioutil.ReadAll(r.Body)
+			// bodyString = string(bodyBytes)
+		} else {
+			STATUS.SmokeStatus.OK = false
+		}
+		STATUS.SmokeStatus.Last = time.Now()
 	}
-	STATUS.SmokeStatus.Last = time.Now()
 }
 
 func (s *Status) checkAppHealth() {
 	r, err := http.Get(SETTINGS.Target.HealthEndpoint)
 	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer r.Body.Close()
-	if r.StatusCode == 200 {
-		STATUS.HealthStatus.OK = true
-	} else {
 		STATUS.HealthStatus.OK = false
+	} else {
+		defer r.Body.Close()
+		if r.StatusCode == 200 {
+			STATUS.HealthStatus.OK = true
+		} else {
+			STATUS.HealthStatus.OK = false
+		}
+		STATUS.HealthStatus.Last = time.Now()
 	}
-	STATUS.HealthStatus.Last = time.Now()
 }
 
 func (s *Status) startHealthMonitor() {
