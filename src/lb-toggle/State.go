facab3be5f0f21ec195db9632bb5a4cd3b07ed4f
Joe Vacovsky <vacovsky@gmail.com>
2017-03-17 21:28:44 -0700

    tweaks

--- src/lb-toggle/State.go
+++ src/lb-toggle/State.go
@@ -9,7 +9,7 @@ import (
 // State represents the way the application handles administratively on/off
 type State struct {
 	OK                  bool   `'json:"ok"`                  // value looked at for true app status
-	StartupState        string `json:"startup_state"`        // whether State.OK should be true or false upon monitor startup
+	StartupState        bool   `json:"startup_state"`        // whether State.OK should be true or false upon monitor startup
 	PersistState        bool   `json:"persist_state"`        // if administratively up or down, will store that in a file and pull it in upon settings reload
 	AdministrativeState string `json:"administrative_state"` // something
 }
